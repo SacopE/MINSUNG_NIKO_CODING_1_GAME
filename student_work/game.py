5de1aed3f1aa01af68ad243a8460c6f4b89cc2fd
SacopE <s977860@student.knoxschools.org>
2026-02-26 16:52:19 +0000

    Added Phase Two

--- student_work/game.py
+++ student_work/game.py
@@ -1,7 +1,56 @@
 import curses
 
 game_data = {
-    # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
+    'width': 5,
+    'height': 5,
+    'player': {"x": 0, "y": 0, "score": 0, "energy": 10, "max_energy": 10},
+    'eagle_pos': {"x": 4, "y": 4},
+    'collectibles': [
+        {"x": 2, "y": 1, "collected": False},
+    ],
+
+
+    # ASCII icons
+    'turtle': "\U0001F422",
+    'eagle_icon': "\U0001F985",
+    'obstacle': "\U0001FAA8 ",
+    'empty': "  "
+    'apple': '\U0001F34E'
+
+}
+
+def draw_board(stdscr):
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_WHITE, -1)
+
+    stdscr.clear()
+    for y in range(game_data['height']):
+        row = ""
+        for x in range(game_data['width']):
+            # Player
+            if x == game_data['player']['x'] and y == game_data['player']['y']:
+                row += game_data['turtle']
+            # Eagle
+            elif x == game_data['eagle_pos']['x'] and y == game_data['eagle_pos']['y']:
+                row += game_data['eagle_icon']
+            # Obstacles
+            elif any(o['x'] == x and o['y'] == y for o in game_data['obstacles']):
+                row += game_data['obstacle']
+            # Collectibles
+            elif any(c['x'] == x and c['y'] == y and not c['collected'] for c in game_data['collectibles']):
+                row += game_data['leaf']
+            else:
+                row += game_data['empty']
+        stdscr.addstr(y, 0, row, curses.color_pair(1))
+
+    stdscr.refresh()
+    stdscr.getkey()  # pause so player can see board
+
+curses.wrapper(draw_board)
+
+game_data = {
+    
 }
 
 def draw_board(screen):
